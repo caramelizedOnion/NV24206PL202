3203c4a312da86cbffe676d03ec724a30cafaacf
Ali Mohamed Amralla <xxalixx780@gmail.com>
2026-03-04 08:32:26 +0000

    commit

---
+++ task11.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+arr = np.array([10, 20, 30, 40, 50, 60, 70])
+print("Original Array:", arr)
+
+print("Slice [0:7]:", arr[0:7])
+
+print("Slice [:4]:", arr[:4])
+
+print("Slice [:-2]:", arr[:-2])
+
+print("Slice [::2]:", arr[::2])
